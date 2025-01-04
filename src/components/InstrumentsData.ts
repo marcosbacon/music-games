04de5da59901b4d51bce0db2f4a2f51fe2a0b071
Marcos Bacon <kakobacon@gmail.com>
2025-01-03 21:47:33 -0300

    The / will fix it? No idea.

--- src/components/InstrumentsData.ts
+++ src/components/InstrumentsData.ts
@@ -7,33 +7,33 @@ export interface Instrument {
 const instruments: Instrument[] = [
   {
     name: "Piano",
-    image: "music-games/assets/instruments/piano.png",
-    sound: "music-games/assets/instruments/piano.wav",
+    image: "/music-games/assets/instruments/piano.png",
+    sound: "/music-games/assets/instruments/piano.wav",
   },
   {
     name: "Guitar",
-    image: "music-games/assets/instruments/guitar.png",
-    sound: "music-games/assets/instruments/guitar.wav",
+    image: "/music-games/assets/instruments/guitar.png",
+    sound: "/music-games/assets/instruments/guitar.wav",
   },
   {
     name: "Drums",
-    image: "music-games/assets/instruments/drums.png",
-    sound: "music-games/assets/instruments/drums.wav",
+    image: "/music-games/assets/instruments/drums.png",
+    sound: "/music-games/assets/instruments/drums.wav",
   },
   {
     name: "Cello",
-    image: "music-games/assets/instruments/cello.png",
-    sound: "music-games/assets/instruments/cello.mp3",
+    image: "/music-games/assets/instruments/cello.png",
+    sound: "/music-games/assets/instruments/cello.mp3",
   },
   {
     name: "Harp",
-    image: "music-games/assets/instruments/harp.png",
-    sound: "music-games/assets/instruments/harp.wav",
+    image: "/music-games/assets/instruments/harp.png",
+    sound: "/music-games/assets/instruments/harp.wav",
   },
   {
     name: "Xylophone",
-    image: "music-games/assets/instruments/xylophone.png",
-    sound: "music-games/assets/instruments/xylophone.wav",
+    image: "/music-games/assets/instruments/xylophone.png",
+    sound: "/music-games/assets/instruments/xylophone.wav",
   },
 ];
 
